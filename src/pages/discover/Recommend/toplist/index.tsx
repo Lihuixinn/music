5068090ad74669836e552ce0df2132fbf30fd248
xinxinmeiyouxin <2413625541@qq.com>
2023-07-28 19:16:50 +0800

    11

--- src/pages/discover/Recommend/toplist/index.tsx
+++ src/pages/discover/Recommend/toplist/index.tsx
@@ -1,11 +1,49 @@
-import React, { Fragment } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { List, Spin } from 'antd';
 
-const Toplist:React.FC= () =>{
-    return (
-        <Fragment>
-
-        </Fragment>
-    )
+interface Song {
+  id: number;
+  name: string;
+  artist: string;
 }
 
-export default Toplist;
+const Billboard: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [songs, setSongs] = useState<Song[]>([]);
+
+  useEffect(() => {
+    // 发起HTTP请求获取榜单数据
+    axios.get('https://your-api-endpoint.com/billboard')
+      .then(response => {
+        setSongs(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error: ', error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <Spin />
+      ) : (
+        <List
+          dataSource={songs}
+          renderItem={(song: Song) => (
+            <List.Item>
+              <List.Item.Meta
+                title={song.name}
+                description={song.artist}
+              />
+            </List.Item>
+          )}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Billboard;
