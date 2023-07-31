af1466d8dc2153452ae95e4bc92cff3cc9a37929
xinxinmeiyouxin <2413625541@qq.com>
2023-07-31 11:12:13 +0800

    修改点击事件

--- src/components/Header/bottomNav/index.tsx
+++ src/components/Header/bottomNav/index.tsx
@@ -1,44 +1,63 @@
-import React, {  } from "react";
+import React, { useState }  from "react";
 import "../styles/bottom-nav.css"
 import { NavLink } from "react-router-dom";
-// import {SearchOutlined} from "@ant-design/icons"
-const redNav:{name:string,path:string} [] = [
+import { log } from "console";
+// import styled from 'styled-components';
+
+
+
+const Right:React.FC=()=>{
+  const [redNav,setRedNav] =useState( [
     {
       name: '推荐',
       path: '/discover',
+      isClick:true,
     },
     {
       name: '排行榜',
       path: '',
+      isClick:false,
     },
     {
       name: '歌单',
       path: '', 
+      isClick:false,
     },
     {
       name: '主播电台',
       path: '',
+      isClick:false,
     },
     {
       name: '歌手',
       path: '',
+      isClick:false,
     },
     {
       name: '新碟上架',
       path: '',
+      isClick:false,
     }
-  ]
+  ])
+
+    const handleClick=(index:number)=>{
+      const updatedNav = redNav.map((item, i) => ({  
+        ...item,
+        isClick: i === index
+      }));
+
+      setRedNav(updatedNav);
+    };
 
 
-const Right:React.FC=()=>{
         return(
            <div className='rednav'>
                 <div className='red-conent'>
                 <ul className="red">
                   {
-                    redNav.map((redNav)=>{
-                        return <li key={redNav.name} className="redList">
-                            <NavLink to="/" className="redlink">{redNav.name}</NavLink>
+                    redNav.map((redNav,index)=>{
+                        return <li key={index} className="redList" >
+                            <NavLink to="/" className="redlink" onClick={()=>handleClick(index)} style={{backgroundColor:redNav.isClick?"#9B0909":'#c20c0c'}}> {redNav.name}</NavLink >
                         </li>
                     })
                   }
